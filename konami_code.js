84ea597eed6a8f79aed09588ea270e571028f0ee
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-07-26 05:08:02 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,36 +13,17 @@ const codes = [
 
 
 
-    function init() {
-      // your code here
-
-      let i = 0
-
-      document.body.addEventListener('keydown', (e) => {
-        var pressed = e.key
-        if(pressed == codes[i]) {
-          i++
-        } else {
-          i = 0
-        }
-
-        if(i == 10) {
-          alert('Congratulations, you have unlocked the Konami Code!')
-          i = 0
-        }
-      })
-    }
-// let orderedCounter = 0
-//  document.body.addEventListener('keydown', function(e){
-//   let pressed = e.key
-//   if(pressed === codes[orderedCounter]){
-// 	orderedCounter++
-//   if(orderedCounter === codes.length){
-// 		window.alert("Hurray!")
-//     orderedCounter = 0
-// 	}
-// } else {
-// 	orderedCounter = 0
-// }
-//  })
-// }
+  function init(){
+let orderedCounter = 0
+ document.body.addEventListener('keydown', function(e){
+  if(e.key === codes[orderedCounter]){
+	orderedCounter++
+  if(orderedCounter === codes.length){
+		alert("Hurray!")
+    orderedCounter = 0
+	}
+} else {
+	orderedCounter = 0
+}
+ })
+}
